4cfebd00f4137832e57fc1dae24ed509f0378cd0
abilashabi8044-crypto <abilashabi8044@gmail.com>
2026-09-16 17:49:57 +0530

    comiit

--- src/Pages/loginpopup.jsx
+++ src/Pages/loginpopup.jsx
@@ -3,7 +3,7 @@ import asset1 from '../assets/Loginpopup/Asset 1.png';
 import vector1 from '../assets/Loginpopup/Vector1.png';
 import vector2 from '../assets/Loginpopup/Vector2.png';
 import closeBtn from '../assets/Loginpopup/close.png';
-import vector3 from '../assets/Loginpopup/vector3.png';
+import vector3 from '../assets/Loginpopup/Vector3.png';
 
 const LoginPopup = ({ onClose }) => {
     const [email, setEmail] = useState('');
